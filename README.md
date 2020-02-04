1cbd9531b51d8a26b6681a3514c4ce72827f8521
Ezoterica <58933577+Ezoterica@users.noreply.github.com>
2020-02-04 16:12:18 +0200

    add all

---
+++ README.md
@@ -0,0 +1,2 @@
+#Mans projekts
+###Alberts
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/index")
-
 def getIndex():
   return "<h1><a href='/about'>Alberts Korols</a></h1>"
 
@@ -26,5 +25,45 @@ def contact():
 def contact2():
   return render_template('contact.html', phone = 12345678)
 
+@app.route('/params')
+def params():
+  return request.args
+
+@app.route('/post_req', methods = ['POST'])
+def post_req():
+  return request.args
   
-app.run(host = '0.0.0.0', port=8020)
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
+
+@app.route('/write_file', methods = ['POST'])
+def write_to_file():
+  if request.content_type == 'application/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data '])
+    return f"Add line {contentJSON['data']} to file." 
+  else:
+    return f"Invalid request {request.content_type} not supported!"
+
+@app.route('/file', methods = ['GET', 'POST'])
+def fileWork():
+  if request.method == 'GET':
+    return read_from_file()
+  elif request.method == 'POST':
+    return write_to_file()
+  else:
+    return f"Method {request.method} not supported!"
+
+@app.route('/json')
+def json_get():
+  list = []
+  list.append("value1")
+  list.append("value2")
+  list.append("value3")
+  return jsonify({"data":list})
+    
+if __name__ == "__main__":
+  app.run(host = '0.0.0.0', port=8020, threaded=True, debug=True)
+
